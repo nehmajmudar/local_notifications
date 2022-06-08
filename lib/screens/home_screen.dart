5c469c39924c7f732aa956d00167bb001fb4e688
nehmajmudar <neh.majmudar@bacancy.com>
2022-06-08 18:06:59 +0530

    Updated the code.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:local_notifications/main.dart';
@@ -16,6 +18,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   bool isSelected=false;
   bool isSimpleSelected=false;
+  bool isPeriodSelected=false;
+  bool isInsistent=false;
+  bool didProgress=false;
 
   @override
   void initState() {
@@ -63,87 +68,169 @@ class _HomeScreenState extends State<HomeScreen> {
         androidAllowWhileIdle: true);
   }
 
+  Future<void> showPeriodicNotification()async{
+    await flutterLocalNotificationsPlugin.periodicallyShow(234, "periodic", "Periodic notification", RepeatInterval.everyMinute,
+      const NotificationDetails(
+        android: AndroidNotificationDetails("periodic channel", "period",channelDescription: "channel_descri",icon: '@mipmap/ic_launcher')
+      )
+    );
+  }
+
+  Future<void> showProgressNotification() async {
+    const int maxProgress = 6;
+    for (int i = 0; i <= maxProgress; i++) {
+      await Future<void>.delayed(const Duration(seconds: 2), () async {
+        final AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails('channel_id', 'Channel Name', channelDescription: 'progress_channel',
+            channelShowBadge: false,
+            importance: Importance.max,
+            priority: Priority.high,
+            onlyAlertOnce: true,
+            showProgress: true,
+            maxProgress: maxProgress,
+            progress: i);
+        final NotificationDetails notificationDetails = NotificationDetails(android: androidNotificationDetails);
+        await flutterLocalNotificationsPlugin.show(0, 'Flutter Local Notification', 'Flutter Progress Notification',
+            notificationDetails);
+      });
+    }
+  }
+
+
+
+  Future<void> showInsistentNotification()async{
+    int insistentFlag=5;
+    await flutterLocalNotificationsPlugin.show(156, "insistent", "Insistent notification",
+      NotificationDetails(
+        android: AndroidNotificationDetails("insistent channel", "Insistence",channelDescription: "channel_insistence",
+            importance: Importance.high,
+            priority: Priority.high,
+            ticker: 'ticker',
+            additionalFlags: Int32List.fromList(<int>[insistentFlag]),
+            icon: '@mipmap/ic_launcher')
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Local notifications"),
+        title: const Text("Press for demo of notification"),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: (){
-                setState(() {
-                  isSelected=!isSelected;
-                });
-                print("SImple button pressed");
-                showNotification();
-                print("SImple button pressed later");
-              },
-              child: Container(
-                margin: const EdgeInsets.only(top: 200),
-                child: const Text("Simple",style: TextStyle(fontSize: 50)),
-                decoration: isSelected?const BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(Radius.circular(20))
-                ):const BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.all(Radius.circular(20))
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: (){
-                setState(() {
-                  isSimpleSelected=!isSimpleSelected;
-                });
-                print("Scheduled notification button pressed");
-                scheduledNotification();
-                print("Scheduled notification button pressed later");
-              },
-              child: Container(
-                margin: const EdgeInsets.only(top: 40),
-                child: const Text("Scheduled",style: TextStyle(fontSize: 50)),
-                decoration: isSimpleSelected?const BoxDecoration(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    isSelected=!isSelected;
+                  });
+                  print("SImple button pressed");
+                  showNotification();
+                  print("SImple button pressed later");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 80),
+                  child: const Text("Simple",style: TextStyle(fontSize: 50)),
+                  decoration: isSelected?const BoxDecoration(
                     color: Colors.red,
                     borderRadius: BorderRadius.all(Radius.circular(20))
-                ):const BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ):const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
                 ),
               ),
-            ),
-          ],
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    isSimpleSelected=!isSimpleSelected;
+                  });
+                  print("Scheduled notification button pressed");
+                  scheduledNotification();
+                  print("Scheduled notification button pressed later");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: const Text("Scheduled",style: TextStyle(fontSize: 50)),
+                  decoration: isSimpleSelected?const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ):const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    isPeriodSelected=!isPeriodSelected;
+                  });
+                  print("Period notification button pressed");
+                  showPeriodicNotification();
+                  print("Period notification button pressed later");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: const Text("Periodic",style: TextStyle(fontSize: 50)),
+                  decoration: isPeriodSelected?const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ):const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    isInsistent=!isInsistent;
+                  });
+                  print("Period notification button pressed");
+                  showInsistentNotification();
+                  print("Period notification button pressed later");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: const Text("Insistent",style: TextStyle(fontSize: 50)),
+                  decoration: isInsistent?const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ):const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    didProgress=!didProgress;
+                  });
+                  print("Progress notification button pressed");
+                  showProgressNotification();
+                  print("Progress notification button pressed later");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: const Text("Progress",style: TextStyle(fontSize: 50)),
+                  decoration: didProgress?const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ):const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      // body: Center(
-      //   child: Column(
-      //     children: [
-      //       GestureDetector(
-      //         onTap: (){
-      //           showSimpleNotification();
-      //         },
-      //         child: Container(
-      //           margin: const EdgeInsets.only(bottom: 40),
-      //           color: Colors.orangeAccent,
-      //           child: const Text('Simple notification'),
-      //         ),
-      //       ),
-      //       GestureDetector(
-      //         onTap: (){
-      //           scheduleNotification();
-      //         },
-      //         child: Container(
-      //           margin: const EdgeInsets.only(bottom: 40),
-      //           color: Colors.orangeAccent,
-      //           child: const Text('Scheduled notification'),
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
